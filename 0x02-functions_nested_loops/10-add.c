1f9e885f66e99ef9567a7e0e2c80a611419dbb6e
Wilson Mutua <willymathuva@gmail.com>
2023-03-26 14:49:41 +0000

    function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add -Prototype to add two integers
+ *@n: integer passed
+ *@m: integers passed to the function
+ *
+ * Return: retval added value
+ */
+
+int add(int n, int m)
+{
+	int retval;
+
+	retval = n + m;
+	return (retval);
+}
