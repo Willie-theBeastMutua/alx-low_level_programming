34de543b75712e818289f5cd9d4c75f2d2bbdfee
Wilson Mutua <willymathuva@gmail.com>
2023-04-06 09:38:41 +0000

    function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int c = 0;
 	int i;
 	int comb;
+	int count = 0;
 
+	while(dest[count] != '\0')
+		count++;
 	while (src[c] != '\0')
 		c++;
 	if (n > c)
@@ -27,5 +30,7 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	if (n > c)
 		dest[i] = '\0';
+	else
+		dest[count] = '\0';
 	return (dest);
 }
