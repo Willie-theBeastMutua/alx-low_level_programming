cc17a3b4481b2e6222eb3e47046fd0b677826d6a
Wilson Mutua <willymathuva@gmail.com>
2023-04-06 09:31:04 +0000

    function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -24,9 +24,8 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < comb; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 	if (n > c)
-		dest[i - 1] = '\0';
+		dest[i] = '\0';
 	return (dest);
 }
