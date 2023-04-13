a77b34791b86d6c7a1e22f4f39240ffd0164c0f2
Wilson Mutua <willymathuva@gmail.com>
2023-04-13 12:00:57 +0000

    function that allocates memory for an array, using malloc.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = ar;
 	if (ar == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		ptr[i] = 0;
 	return (ar);
 }
