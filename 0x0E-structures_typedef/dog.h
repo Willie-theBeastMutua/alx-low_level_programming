2a92dfcd3a136789a97097477fb25b7bb477a3ff
Wilson Mutua <willymathuva@gmail.com>
2023-05-01 17:18:36 +0000

    new type struct dog with the following elements:

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - struct for dog
+ *@name: First member
+ *@age: Second member
+ *@owner: Third member
+ *
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
