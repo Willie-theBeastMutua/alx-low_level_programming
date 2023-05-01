a1bdbeea2df999f6b80eb6a4dd41e2ace38f5076
Wilson Mutua <willymathuva@gmail.com>
2023-05-01 17:58:33 +0000

    function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,26 +13,32 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	Allocate memory for the new dog
-		dog_t *dog = malloc(sizeof(dog_t));
+	dog_t *dog;
+	int i;
+	int j;
+
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(strlen(name) + 1);
+	i = strlen(name);
+	j = strlen(owner);
+
+	dog->name = malloc(sizeof(char) * (i + 1));
 	if (dog->name == NULL)
-	{
-		free(dog);
 		return (NULL);
-	}
-	strcpy(dog->name, name);
-	dog->owner = malloc(strlen(owner) + 1);
+
+	dog->owner = malloc(sizeof(char) * (j + 1));
 	if (dog->owner == NULL)
 	{
-		free(dog->name);
 		free(dog);
+		free(dog->name);
 		return (NULL);
 	}
+
+	strcpy(dog->name, name);
 	strcpy(dog->owner, owner);
 	dog->age = age;
+
 	return (dog);
 }
