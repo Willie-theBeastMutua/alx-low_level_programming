ce9adedd046180c5529ba21e4ed36b984d91bdeb
Wilson Mutua <willymathuva@gmail.com>
2023-06-09 13:43:41 +0000

    4-add argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,36 +13,32 @@
 int main(int argc, char *argv[])
 {
 
+	int sum = 0;
 	int i;
 	int num;
-	int sum;
 
-	if (argc == 1)
+	for (i = 1; i < argc; i++)
+
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		int j = 0;
+
+		while (argv[i][j] != '\0')
 		{
-			if (isdigit(argv[i][0]) == 0)
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				num = atoi(argv[i]);
-				if (num < 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
-				sum += num;
-			}
+			j++;
 		}
-		printf("%d\n", sum);
-
+		num = atoi(argv[i]);
+		if (num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
 	}
+	printf("%d\n", sum);
 	return (0);
 }
