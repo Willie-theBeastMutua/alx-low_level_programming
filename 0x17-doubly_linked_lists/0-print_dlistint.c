34855113377da454ccb5e79f07556b0579a9819b
Willy Mutua <willymathuva@gmail.com>
2023-08-30 00:39:09 +0300

    0x17. C - Doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* print_dlistint - prints all the elements
+* @h: a pointer to struct dlistint_t
+* Return: size_t
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
